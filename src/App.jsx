c193989aeb971e639e79f7c2ca5245a957767393
bauti15 <bautistapiovani0@gmail.com>
2023-06-21 18:32:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+
+
+function App() {
+
+return(
+  <main className="bg-zinc-900 h-sc
+  ">
+<div className="container mx-auto p-10">
+
+<TaskForm></TaskForm>
+<TaskList></TaskList>
+</div>
+
+
+  </main>
+)
+
+
+
+
+}
+export default App;
